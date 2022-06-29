499e14e93f33d81ff36534d1649c01b5c9e231ec
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-06-30 00:12:48 +0600

    very easy one. previously solved Alhamdulillah

---
+++ implementationconstruc/WrongSubtraction.java
@@ -0,0 +1,16 @@
+package striverCpSheet.implementationconstruc;
+
+import java.util.Scanner;
+
+public class WrongSubtraction {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        for (int i = 0; i < k; i++) {
+            if(n%10==0) n/=10;
+            else n--;
+        }
+        System.out.println(n);
+    }
+}
