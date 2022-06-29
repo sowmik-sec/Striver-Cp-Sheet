ea6a8a295a9c10cae973a9e77414c54cfdd436b4
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-06-30 00:23:35 +0600

    Alhamdulillah

--- implementationconstruc/NearlyLuckyNumber.java
+++ implementationconstruc/NearlyLuckyNumber.java
@@ -1,5 +1,18 @@
 package striverCpSheet.implementationconstruc;
 
+import java.util.Scanner;
+
 public class NearlyLuckyNumber {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i)=='4' || s.charAt(i)=='7') {
+                cnt++;
+            }
+        }
+        if(cnt==4 || cnt==7) System.out.println("YES");
+        else System.out.println("NO");
+    }
 }
