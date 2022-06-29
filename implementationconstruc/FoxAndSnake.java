1c4778477db0d91d1ebb406a4e54b8a132b6d67d
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-06-29 15:13:45 +0600

    This is an easy problem. I've to think more carefully. Solved Alhamdulillah

--- implementationconstruc/FoxAndSnake.java
+++ implementationconstruc/FoxAndSnake.java
@@ -7,25 +7,25 @@ public class FoxAndSnake {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int m = in.nextInt();
-        boolean ahs = false;
+        boolean ahs = false, first = true, last = false;
         int index = 1;
+        int row = -1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if(i%2==0) {
                     System.out.print("#");
                 }
-                else if(j==m-1 && ahs==false) {
-                    System.out.print("#");
-                    ahs = true;
+                else{
+                    if(j==0 && ((i+1)%4==0)) {
+                        System.out.print("#");
+                    }
+                    else if(j==m-1 && (i+1)%4!=0) {
+                        System.out.print("#");
+                    }
+                    else System.out.print(".");
                 }
-                else if(j==0 && ahs == true) {
-                    System.out.print("#");
-                    index = i;
-                }
-                else System.out.print(".");
             }
             System.out.println();
-            //if()
         }
     }
 }
