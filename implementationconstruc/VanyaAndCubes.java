b6bd3402a7041bdcea19c46c789c46f9cf41119f
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-06-30 11:58:21 +0600

    Alhamdulillah

--- implementationconstruc/VanyaAndCubes.java
+++ implementationconstruc/VanyaAndCubes.java
@@ -1,2 +1,20 @@
-package striverCpSheet.implementationconstruc;public class VanyaAndCubes {
+package striverCpSheet.implementationconstruc;
+
+import java.util.Scanner;
+
+public class VanyaAndCubes {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int cnt = 1;
+        int cur = 0;
+
+        while(cur<n) {
+            cur += cnt*(cnt+1)/2;
+            cnt++;
+            //System.out.println(cur);
+        }
+        if(cur==n) System.out.println(cnt-1);
+        else System.out.println(cnt-2);
+    }
 }
