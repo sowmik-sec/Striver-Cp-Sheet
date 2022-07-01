fe1084967cd91f4e9ae61c3f9499d410f0aaac2a
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-07-01 15:55:27 +0600

    This is more clear & understandable code. Alhamdulillah

--- implementationconstruc/AssigningToClasses.java
+++ implementationconstruc/AssigningToClasses.java
@@ -1,6 +1,5 @@
 package striverCpSheet.implementationconstruc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -16,24 +15,7 @@ public class AssigningToClasses {
                 a[i] = in.nextInt();
             }
             Arrays.sort(a);
-            
-            ArrayList<Integer> list1 = new ArrayList<>();
-            ArrayList<Integer> list2 = new ArrayList<>();
-            if(n%2==1) {
-                for (int i = 0; i < n*2; i++) {
-                    if(i%2==0) list1.add(a[i]);
-                    else list2.add(a[i]);
-                }
-                System.out.println(Math.abs(list1.get(list1.size()/2)-list2.get(list2.size()/2)));
-            }
-            else {
-                list2.add(a[n]);
-                for (int i = 0; i < n*2; i++) {
-                    if(i==n) continue;
-                    list1.add(a[i]);
-                }
-                System.out.println(Math.abs(list1.get(list1.size()/2)-list2.get(list2.size()/2)));
-            }
+            System.out.println(a[n]-a[n-1]);
         }
     }
 }
