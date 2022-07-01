0475d005c7799b68508474a01d6365b8470345c4
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-07-01 17:49:53 +0600

    Solved Alhamdulillah

--- implementationconstruc/NumbersOnWhiteBoard.java
+++ implementationconstruc/NumbersOnWhiteBoard.java
@@ -8,10 +8,16 @@ public class NumbersOnWhiteBoard {
         int t = in.nextInt();
         for (int i = 0; i < t; i++) {
             int n = in.nextInt();
-            if(n%2==0) {
-
+            System.out.println(2);
+            if(n==2) {
+                System.out.println("2 1");
+                continue;
             }
-
+            System.out.println(n+" "+(n-2));
+            System.out.println((n-1)+" "+(n-1));
+            for (int j = n-1; j >=3 ; j--) {
+                System.out.println(j+" "+(j-2));
+            }
         }
     }
 }
