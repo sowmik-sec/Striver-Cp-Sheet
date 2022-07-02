3006dc88a9a42709b9169e87c138611fd7869bf5
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-07-02 09:56:50 +0600

    Tried hard and solved Alhamdulillah

--- implementationconstruc/Buttons.java
+++ implementationconstruc/Buttons.java
@@ -1,2 +1,17 @@
-package striverCpSheet.implementationconstruc;public class Buttons {
+package striverCpSheet.implementationconstruc;
+
+import java.util.Scanner;
+
+public class Buttons {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int ans = 0;
+        for (int i = n; i >0 ; i--) {
+            int temp = n-i;
+            int temp2 = i-1;
+            ans += temp*temp2+i;
+        }
+        System.out.println(ans);
+    }
 }
