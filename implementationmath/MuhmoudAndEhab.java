2ff68a846646c4345c72c6bd30b11eb5959bca13
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-07-03 12:36:41 +0600

    Extremely easy problem. Alhamdulillah

---
+++ implementationmath/MuhmoudAndEhab.java
@@ -0,0 +1,12 @@
+package striverCpSheet.implementationmath;
+
+import java.util.Scanner;
+
+public class MuhmoudAndEhab {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if(n%2==0) System.out.println("Mahmoud");
+        else System.out.println("Ehab");
+    }
+}
