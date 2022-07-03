d0a4561aaca379ce16d2ccb778db02b44030da4d
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-07-03 10:55:44 +0600

    Very easy one. Alhamdulillah

---
+++ implementationconstruc/BalancedRatingChanges.java
@@ -0,0 +1,2 @@
+package striverCpSheet.implementationconstruc;public class BalancedRatingChanges {
+}
---
+++ implementationmath/IchihimeAndTriangle.java
@@ -0,0 +1,18 @@
+package striverCpSheet.implementationmath;
+
+import java.util.Scanner;
+
+public class IchihimeAndTriangle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while(t>0) {
+            t--;
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int c = in.nextInt();
+            int d = in.nextInt();
+            System.out.println(b+" "+c+" "+c);
+        }
+    }
+}
