ad1e7509aa10751a0e2ceda5ac426ffc89bc2721
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-07-04 17:29:37 +0600

    Solved Alhamdulillah

---
+++ implementationmath/DominoPiling.java
@@ -0,0 +1,12 @@
+package striverCpSheet.implementationmath;
+
+import java.util.Scanner;
+
+public class DominoPiling {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println((a*b)/2);
+    }
+}
