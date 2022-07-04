eec9e4bd0074acdde39188e2fd8f540ab7b045e8
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-07-04 10:14:15 +0600

    This is a very easy problem. But I thought it hard. I spoiled a lot of time solving this problem. Alhamdulillah for everything

--- implementationmath/LCMPromlem.java
+++ implementationmath/LCMPromlem.java
@@ -1,2 +1,19 @@
-package striverCpSheet.implementationmath;public class LCMPromlem {
+package striverCpSheet.implementationmath;
+
+import java.util.Scanner;
+
+public class LCMPromlem {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while(t-->0) {
+            long l = in.nextInt();
+            long r = in.nextInt();
+            long x = -1, y = -1;
+            if(l*2>r) {
+                System.out.println("-1 -1");
+            }
+            else System.out.println(l+" "+l*2);
+        }
+    }
 }
